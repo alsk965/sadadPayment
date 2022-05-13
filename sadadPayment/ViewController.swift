d39c3ea1b52eca7fbdb3540ab3838034d8cad25b
Aly Fawzy <ali.fawzy@orchtech.com>
2022-05-14 01:08:04 +0200

    Initial Commit

---
+++ sadadPayment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  sadadPayment
+//
+//  Created by aly fawzy on 14/05/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
